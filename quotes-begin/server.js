a43403c4ddad8c048b8da09d31fbb3452420c1b5
J Silverstein <jess.pebble@gmail.com>
2017-08-21 13:53:24 -0400

    add comments

--- quotes-begin/server.js
+++ quotes-begin/server.js
@@ -1,13 +1,21 @@
+// import dependencies
 const express = require('express');
 const logger = require('morgan');
 
+// initialize app
 const app = express();
 
+// set up port and listen
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
+// set up logger middleware
+app.use(logger('dev'));
+
+
+// index route
 app.get('/', (req, res) => {
   res.send('HELLO WORLD!');
 });
