03c16760f42178991a8d3e574427e276dd0af4cf
Katherine Shishkina <shishkina@users.noreply.github.com>
2017-11-20 14:02:57 -0500

    move things around 2.0

--- quotes-begin/server.js
+++ quotes-begin/server.js
@@ -1,13 +1,16 @@
 // import dependencies
 const express = require('express');
-// this middleware will print to console
-const logger = require('morgan');
+// set up logger
+
+// set up port and listen (good practice to place it all the way at the bottow of the file)
+const PORT = 3000;
+
 
 // initialize app
 const app = express();
 
 // set up logger middleware to be used in dev environment
-app.use(logger('dev'));
+
 
 
 // index route
@@ -15,8 +18,6 @@ app.get('/', (req, res) => {
   res.send('HELLO WORLD! We are Hamilton');
 });
 
-// set up port and listen (good practice to place it all the way at the bottow of the file)
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
