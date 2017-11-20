ab19279b3649dd1f7c19b070482199b62a88c9c3
Katherine Shishkina <shishkina@users.noreply.github.com>
2017-11-20 12:34:15 -0500

    move things around

--- quotes-begin/server.js
+++ quotes-begin/server.js
@@ -1,21 +1,22 @@
 // import dependencies
 const express = require('express');
+// this middleware will print to console
 const logger = require('morgan');
 
 // initialize app
 const app = express();
 
-// set up port and listen
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
-
-// set up logger middleware
+// set up logger middleware to be used in dev environment
 app.use(logger('dev'));
 
 
 // index route
 app.get('/', (req, res) => {
-  res.send('HELLO WORLD!');
+  res.send('HELLO WORLD! We are Hamilton');
+});
+
+// set up port and listen (good practice to place it all the way at the bottow of the file)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
