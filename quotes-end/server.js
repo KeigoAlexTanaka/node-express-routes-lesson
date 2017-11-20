7a8b7de3f98c57b61b7fd47edc0c65df75482bbe
Katherine Shishkina <shishkina@users.noreply.github.com>
2017-11-20 12:36:52 -0500

    move things around

--- quotes-end/server.js
+++ quotes-end/server.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const logger = require('morgan');
+const PORT = process.env.PORT || 3000;
+const quoteRoutes = require('./routes/quote-routes');
 
 const app = express();
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, (req, res) => {
-  console.log(`Listening on port ${PORT}`);
-});
-
+/* use logger in dev environment */
 app.use(logger('dev'));
 
+/* set up a home route*/
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-const quoteRoutes = require('./routes/quote-routes');
+/* middleware to use quotes routes to display the quote data */
 app.use('/quotes', quoteRoutes);
 
+/* error handler */
 app.use('*', (req, res) => {
   res.status(404).json('Not Found!');
+});
+
+app.listen(PORT, (req, res) => {
+  console.log(`Listening on port ${PORT}`);
 });
